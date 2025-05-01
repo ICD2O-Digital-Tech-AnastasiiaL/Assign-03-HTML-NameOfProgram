e46d6c4a37b8f449d5e394c5f69ba3b0809a4d17
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-05-01 15:01:30 +0000

    Final Update and finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the volume of the triangle
+ */
+function calculateVolume() {
+  // Information about values
+    let length = parseFloat(document.getElementById('length').value);
+    let base = parseFloat(document.getElementById('base').value);
+    let height = parseFloat(document.getElementById('height').value);
+
+  // Calculation
+  let volume = (1/2) * (length * base * height)
+
+  // Display
+  document.getElementById('volume').innerHTML = 'Volume of a triangular prims is: ' + volume + ' cm<sup>3<sup>'
+}
